cd88b67b9fde1266b3ae013242acd97fd9c7ea53
wangxingfeng <wangxingfeng@tigerjoys.com>
2016-06-06 12:14:54 +0800

    初始化

---
+++ src/com/wxfjava/poi/ExcelTest.java
@@ -0,0 +1,41 @@
+package com.wxfjava.poi;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.hssf.record.crypto.Biff8EncryptionKey;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class ExcelTest {
+    
+    public static void main(String[] args) {
+        for (int i=100; i<1000; i++){
+            try {
+                readFile("wangxingfeng"+i);
+            } catch (Exception e) {
+                
+            }
+            
+        }
+        
+    }
+
+    private static void readFile(String password) throws IOException, FileNotFoundException {
+        System.out.println(password);
+        Biff8EncryptionKey.setCurrentUserPassword(password);
+        Workbook workbook = new HSSFWorkbook(new FileInputStream("/Users/WangXingFeng/Downloads/chrome/a.xls")); 
+        
+        Sheet sheet = workbook.getSheetAt(0);
+        Row row = sheet.getRow(0);
+        Cell cell = row.getCell(1);
+        String stringCellValue = cell.getStringCellValue();
+        System.out.println("==================>"+stringCellValue);
+        System.out.println(password);
+    }
+
+}
